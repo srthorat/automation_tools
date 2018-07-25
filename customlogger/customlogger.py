4126c32cd49de85cca96df62976f2cf64afd4572
Sakharam Thorat <sakharam.thorat@greenwavesystems.com>
2018-07-25 21:35:51 +0800

    Added Custom Logger

---
+++ customlogger/customlogger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Description: Custom logger library
+Author : Sakharam Thorat
+Date   : 04/07/2017
+Email  : srt.2011@outlook.com
+
+"""
+
+import logging
+from datetime import datetime
+
+
+def customlogger(logLevel=logging.DEBUG):
+    # Gets the name of the class / method from where this method is called
+    logger = logging.getLogger(__name__)
+    # By default, log all messages
+    logger.setLevel(logging.DEBUG)
+
+    fileHandler = logging.FileHandler('../../logs/'+
+    				datetime.now().strftime('%H_%M_%d_%m_%Y_')+'file.log')
+    fileHandler.setLevel(logLevel)
+
+    formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+    return logger
